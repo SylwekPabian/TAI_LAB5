5ed055d8b9be1e658b5a46ec93a77415a884dc49
SylwekPabian <23137@student.pwsztar.edu.pl>
2019-05-31 14:50:30 +0200

    31-05-2019

---
+++ blog/api/app/DAO/postDAO.js
@@ -0,0 +1,33 @@
+'use strict';
+
+import mongoose from 'mongoose';
+import uniqueValidator from 'mongoose-unique-validator';
+
+import mongoConverter from '../service/mongoConverter';
+
+const postSchema = new mongoose.Schema({
+  name: {type: String},
+  url: {type: String},
+  title: {type: String},
+  text: {type: String}
+}, {
+  collection: 'sp-post'
+});
+postSchema.plugin(uniqueValidator);
+
+const PostModel = mongoose.model('sp-post', postSchema);
+
+async function query() {
+  const result = await postModel.find({});
+  {
+    if (result) {
+      return mongoConverter(result);
+    }
+  }
+}
+
+export default {
+  query: query,
+
+  model: PostModel
+};
